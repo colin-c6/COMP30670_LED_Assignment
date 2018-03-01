6deab403f46e32413b68093508c1ea8dd574a628
colin-c6 <colin.caraher@ucdconnect.ie>
2018-03-01 22:43:48 +0000

    created testing_lightTester.py,wrote tests for limit method and range method, implemented limit and coord_range methods in lightTester.py

--- LED/lightTester.py
+++ LED/lightTester.py
@@ -5,3 +5,46 @@ class LightTester():
 
     def __init__(self,grid_size):
         self.lights = [[False]*grid_size for x in range(grid_size)]
+        
+        
+    def limits(self, command, start, end):
+
+        self.command = command
+        self.start = start
+        self.end = end
+
+        start,end = self.coord_range (self.start,self.end)
+    
+        return""
+    
+    
+    def coord_range (self, coord1,coord2):
+
+        self.start = coord1
+        self.end = coord2
+
+        #making sure point is in range 
+        if self.start[0] < 0:
+            self.start[0] = 0
+        elif self.start[0] > len(self.lights)-1:
+            self.start[0] = len(self.lights)-1
+
+        if self.start[1] < 0:
+            self.start[1] = 0
+        elif self.start[1] > len(self.lights)-1:
+            self.start[1] = len(self.lights)-1
+
+        if self.end[0] < 0:
+            self.end[0] = 0
+        elif self.end[0] > len(self.lights)-1:
+            self.end[0] = len(self.lights)-1
+
+        if self.end[1] < 0:
+            self.end[1] = 0
+        elif self.end[1] > len(self.lights)-1:
+            self.end[1] = len(self.lights)-1
+
+        self.start2=[self.start[0], self.start[1]]
+        self.end2 = [self.end[0], self.end[1]]
+
+        return self.start2,self.end2
---
+++ tests/testing_lightTester.py
@@ -0,0 +1,38 @@
+import unittest
+from LED.parse import *
+from LED.lightTester import *
+
+
+
+class Test_LED(unittest.TestCase):
+
+    #Setting up the grid
+    def setUp(self):
+        self.led = LightTester(3)
+        
+        
+
+         #Testing the given coordinates the limit function will switch the lights    
+    def test_limit_turnOn(self):
+
+        self.test_variable = self.led.limits("turn on", [1,0],[2,2])
+        self.count = 0
+        for r in range (0, len(self.test_variable), 1 ):
+            for j in range (0, len(self.test_variable), 1):
+                if self.test_variable[r][j] == True:
+                    self.count +=1
+        self.assertEqual(self.count, 6,"lights not successfully turned on")  
+        
+         
+
+    #Testing that the coordinates are corrected if they occur outside the range
+    def test_range(self):
+        self.assertEqual(self.led.coord_range([3,4],[4,5]), ([2,2],[2,2]), "Coordinates not corrected to limit")
+        self.assertEqual(self.led.coord_range([-1,1],[-5,-5]), ([0,1],[0,0]),"Coordinates not corrected to limit")
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
