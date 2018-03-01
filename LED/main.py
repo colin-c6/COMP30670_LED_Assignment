b59199dffda406de55971508e2c6e4a72227c20f
colin-c6 <colin.caraher@ucdconnect.ie>
2018-03-01 20:38:31 +0000

    updated main to send each line of input file to parser. Update testing_parser to test that it reads the lines and returns the instructions and created data file with test_data.txt to test parser

--- LED/main.py
+++ LED/main.py
@@ -7,3 +7,17 @@ def main():
     filename = args.input
 
     grid_size = Parser.parse_files(filename) # send to parse_files function
+
+
+    if filename.startswith('http'):
+        with urlopen(filename) as fi:
+            next(fi) #skips first line of file
+            for line in fi:
+                decoded_line = line.decode('utf-8') #decode the lines in the file       
+                instruction, start_point, end_point = Parser.find_instructions(decoded_line)
+  
+    else:
+        with open(filename) as fi:
+            next(fi)
+            for line in fi:      
+                instruction, start_point, end_point = Parser.find_instructions(line)
--- tests/testing_parse.py
+++ tests/testing_parse.py
@@ -5,11 +5,17 @@ class Test_LED(unittest.TestCase):
 
     def test_read_file_local(self):
         file = "./data/test_data.txt"
+        line = "turn on 0,0 through 9,9"
         self.assertEqual(Parser.parse_files(file), 10)
+        self.assertEqual(Parser.find_instructions(line), ("turn on", [0,0], [9,9]))
 
     def test_read_file_network(self):
         file = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
         self.assertEqual(Parser.parse_files(file), 1000)
+
+        line = "turn off 660,55 through 986,197"
+        self.assertEqual(Parser.find_instructions(line), ("turn off", [660,55], [986,197]))
+
 
 
 if __name__ == '__main__':
