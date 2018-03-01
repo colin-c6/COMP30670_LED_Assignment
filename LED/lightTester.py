d5bb3deffa417be453d051ceb0b811808fb9e3ea
colin-c6 <colin.caraher@ucdconnect.ie>
2018-03-01 21:55:19 +0000

    made lightTester.py with LightTester class. makes multidimensional array. updated main to create instance of class and send instructions and coordinates to LighterTester

---
+++ LED/lightTester.py
@@ -0,0 +1,7 @@
+
+
+class LightTester():
+
+
+    def __init__(self,grid_size):
+        self.lights = [[False]*grid_size for x in range(grid_size)]
--- LED/main.py
+++ LED/main.py
@@ -7,7 +7,9 @@ def main():
     filename = args.input
 
     grid_size = Parser.parse_files(filename) # send to parse_files function
-
+    
+    #instigate an instance of the LightTester class
+    grid1 = LightTester(grid_size)
 
     if filename.startswith('http'):
         with urlopen(filename) as fi:
@@ -15,9 +17,11 @@ def main():
             for line in fi:
                 decoded_line = line.decode('utf-8') #decode the lines in the file       
                 instruction, start_point, end_point = Parser.find_instructions(decoded_line)
+                grid1.limits(instruction, start_point, end_point)
   
     else:
         with open(filename) as fi:
             next(fi)
             for line in fi:      
                 instruction, start_point, end_point = Parser.find_instructions(line)
+                grid1.limits(instruction, start_point, end_point)
