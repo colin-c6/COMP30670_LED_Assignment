f8fc4622c5235a198453f589118ba3eac43c8107
colin-c6 <colin.caraher@ucdconnect.ie>
2018-03-01 21:25:55 +0000

    updated parse to parse instructions and coordinates and return them to main.py

--- LED/parse.py
+++ LED/parse.py
@@ -13,3 +13,26 @@ class Parser():
                 first_line = int(f.readline().strip())
 
         return first_line
+    
+    
+    
+        def find_instructions(l):
+
+
+            instruction1 = re.findall(".*(turn on|turn off|switch).*",l)
+            instruction = ''.join(instruction1)
+            coord = re.findall("\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*",l)
+    
+            first_coordinate = []
+            first_x = int(coord[0][0])
+            first_y = int(coord[0][1])
+            first_coordinate += first_x,first_y
+    
+            second_coordinate = []
+            second_x = int(coord[1][0])
+            second_y = int(coord[1][1])
+            second_coordinate += second_x,second_y
+
+
+        return instruction, first_coordinate, second_coordinate
+
