8e261aefb9e83a9638b0c9b13a0aa4c9712434ba
colin-c6 <colin.caraher@ucdconnect.ie>
2018-03-02 01:04:05 +0000

    added additional tests to testing_lightTester to check for different inputs and commands

--- tests/testing_lightTester.py
+++ tests/testing_lightTester.py
@@ -3,7 +3,6 @@ from LED.parse import *
 from LED.lightTester import *
 
 
-
 class Test_LED(unittest.TestCase):
 
     #Setting up the grid
@@ -23,6 +22,30 @@ class Test_LED(unittest.TestCase):
                     self.count +=1
         self.assertEqual(self.count, 6,"lights not successfully turned on")  
         
+    
+    #Testing the given coordinates the limit function will turn off the lights
+    def test_limit_turnOff(self):
+
+        self.test_variable = self.led.limits("turn off", [0,0],[1,1])
+        self.count = 0
+        for r in range (0, len(self.test_variable), 1 ):
+            for j in range (0, len(self.test_variable), 1):
+                if self.test_variable[r][j] == True:
+                    self.count +=1
+        self.assertEqual(self.count, 0,"lights not successfully turned off")
+        
+    
+    #Testing the given coordinates the limit function will switch the lights
+    def test_limit_switch(self):
+
+        self.test_variable = self.led.limits("switch", [0,0],[2,2])
+        self.count = 0
+        for r in range (0, len(self.test_variable), 1 ):
+            for j in range (0, len(self.test_variable), 1):
+                if self.test_variable[r][j] == True:
+                    self.count +=1
+        self.assertEqual(self.count, 9,"lights not successfully switched")
+        
          
 
     #Testing that the coordinates are corrected if they occur outside the range
@@ -30,7 +53,31 @@ class Test_LED(unittest.TestCase):
         self.assertEqual(self.led.coord_range([3,4],[4,5]), ([2,2],[2,2]), "Coordinates not corrected to limit")
         self.assertEqual(self.led.coord_range([-1,1],[-5,-5]), ([0,1],[0,0]),"Coordinates not corrected to limit")
         
-        
+     
+     
+        # testing that the apply function successfully turns on lights on the grid     
+    def test_turn_on(self):
+
+        self.test_variable = self.led.apply("turn on", 0,2)
+
+        self.count = 0
+        for r in range (0, len(self.test_variable), 1 ):
+            for j in range (0, len(self.test_variable), 1):
+                if self.test_variable[r][j] == True:
+                    self.count +=1
+        self.assertEqual(self.count, 1, "Lights not successfully turned on")
+
+
+    # testing that the apply function successfully turns off lights on the grid 
+    def test_turn_off(self):
+
+        self.test_variable = self.led.apply("turn off", 0,2)
+        self.count = 0
+        for r in range (0, len(self.test_variable), 1 ):
+            for j in range (0, len(self.test_variable), 1):
+                if self.test_variable[r][j] == True:
+                    self.count +=1
+        self.assertEqual(self.count, 0, "Lights not successfully turned off")    
      
     # testing that the apply function successfully switches lights on the grid     
     def test_switch(self):
